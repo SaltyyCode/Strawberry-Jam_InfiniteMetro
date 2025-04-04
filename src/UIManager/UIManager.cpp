dbc8f1bf9b4cff524a4293caa487c46d7b5914fe
Saltyy <clement.bouret@epitech.eu>
2025-04-04 19:33:09 +0200

    feat(UIManager.cpp/hpp): methods to render UI

--- src/UIManager/UIManager.cpp
+++ src/UIManager/UIManager.cpp
@@ -6,3 +6,12 @@ void UIManager::render(sf::RenderWindow& window, const std::vector<Station>& sta
         station.render(window);
     }
 }
+
+void UIManager::render(sf::RenderWindow& window, const std::vector<Station>& stations, const std::vector<Line>& lines)
+{
+    for (const auto& line : lines)
+        line.render(window, stations);
+
+    for (const auto& station : stations)
+        station.render(window);
+}
--- src/UIManager/UIManager.hpp
+++ src/UIManager/UIManager.hpp
@@ -2,8 +2,11 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include "../Station/Station.hpp"
+#include "../Line/Line.hpp"
 
 class UIManager {
 public:
     void render(sf::RenderWindow& window, const std::vector<Station>& stations);
+    void render(sf::RenderWindow& window, const std::vector<Station>& stations, const std::vector<Line>& lines);
+
 };
