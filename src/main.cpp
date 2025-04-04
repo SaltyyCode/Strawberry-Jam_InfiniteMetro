8bf332b04bb2ce889108f00846e8ea2637a19e04
Saltyy <clement.bouret@epitech.eu>
2025-04-05 00:37:54 +0200

    feat(main.cpp): ultra clean main

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,13 @@
-#include <SFML/Graphics.hpp>
-#include "GameManager/GameManager.hpp"
-#include "UIManager/UIManager.hpp"
+#include <iostream>
+#include "GameApp.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Infinite Metro");
-    window.setFramerateLimit(60);
-
-    GameManager game(10, 10.f);
-    UIManager ui;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed || 
-                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
-                window.close();
-            }
-        
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                sf::Vector2f pos = window.mapPixelToCoords({ event.mouseButton.x, event.mouseButton.y });
-                game.handleMousePressed(pos);
-            }
-        
-            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
-                sf::Vector2f pos = window.mapPixelToCoords({ event.mouseButton.x, event.mouseButton.y });
-                game.handleMouseReleased(pos);
-            }
-        }
-
-        game.update();
-        game.updateStations();
-        window.clear(sf::Color::White);
-        ui.render(window, game.getStations(), game.getLines());
-        sf::Vector2i mousePixel = sf::Mouse::getPosition(window);
-        sf::Vector2f mouseWorld = window.mapPixelToCoords(mousePixel);
-        game.renderLinePreview(window, mouseWorld);
-
-        window.display();
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <map_name>" << std::endl;
+        return 1;
     }
 
-    return 0;
+    GameApp app;
+    return app.run(argv[1]);
 }
