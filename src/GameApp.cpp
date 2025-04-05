f66f6fb47ab48439ad46bb3b19cca58909c7465d
Saltyy <clement.bouret@epitech.eu>
2025-04-05 17:53:16 +0200

    feat(GameApp): re added line previsualization

--- src/GameApp.cpp
+++ src/GameApp.cpp
@@ -38,6 +38,7 @@ int GameApp::run(const std::string& mapName)
 
         window.clear(sf::Color::Black);
         ui.render(window, game.getStations(), game.getMetroLines(), game.getSelectedLineIndex(), _isPaused, _isMuted);
+        game.renderLinePreview(window, sf::Vector2f(sf::Mouse::getPosition(window)));
         window.display();
     }
 
